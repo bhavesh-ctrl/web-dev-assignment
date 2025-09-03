aca0448bf8f4ff8ed8ca845d4e2e62e8bff721fa
Bhavesh01 <140882383+bhavesh-ctrl@users.noreply.github.com>
2025-09-04 03:01:34 +0530

    Update addSchool.jsx

--- frontend/pages/addSchool.jsx
+++ frontend/pages/addSchool.jsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form';
-import api from '@/utils/api';
+import api from '../utils/api';
 import { useState } from 'react';
 
 export default function AddSchool(){
